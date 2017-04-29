9634a0260e92e0dd232145d2cb5aea917368c00f
Zheng Fu <zf38@cornell.edu>
2017-04-29 14:16:13 -0400

    Refined sober functions and updated in python notebook

--- sobel_func.py
+++ sobel_func.py
@@ -4,7 +4,7 @@ import numpy as np
 def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0,255)):
     xorder = 1
     yorder = 0
-    if orient='y':
+    if orient=='y':
         xorder = 0
         yorder = 1
     sobel = cv2.Sobel(img, cv2.CV_64F, xorder, yorder, ksize=sobel_kernel)
@@ -31,9 +31,17 @@ def dir_thresh(img, sobel_kernel=3, thresh=(0,255)):
     dir_binary[(dir_sobel>thresh[0]) & (dir_sobel<thresh[1])] = 1
     return dir_binary
 
-image = cv2.imread()
+image = cv2.imread('test_images/signs_vehicles_xygrad.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ksize = 3
 
-gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
-grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
-mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
-dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+gradx = abs_sobel_thresh(gray, orient='x', sobel_kernel=ksize, thresh=(0, 255))
+grady = abs_sobel_thresh(gray, orient='y', sobel_kernel=ksize, thresh=(0, 255))
+mag_binary = mag_thresh(gray, sobel_kernel=ksize, thresh=(0, 255))
+dir_binary = dir_thresh(gray, sobel_kernel=ksize, thresh=(0, np.pi/2))
+
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+cv2.imshow('output', combined)
+cv2.waitKey()
